b7cf703945ed7fb17566bde1b297a7a5eb9ec90b
Sunil Gowda S <64064177+sunil3gs98@users.noreply.github.com>
2026-01-04 14:35:55 -0500

    test

---
+++ Test.tf
@@ -0,0 +1,85 @@
+resource "aws_vpc" "Test"{
+
+cidr_block="10.0.0.1/16"
+
+tags={
+
+Name="Provisioners_VPC"
+}
+
+}
+
+
+resource "aws_subnet" "Test1"{
+
+vpc_id =aws_vpc.Test.id
+cidr_block="10.0.0.1/24"
+availability_zone="us-east-1"
+map_public_ip_on_launch=true
+
+
+tags={
+Name="Provisioners_subnet"
+}
+}
+
+resource "aws_internet_gateway" "Pgateway"{
+vpc_id=aws.vpc.Test.id
+tags={
+Name="Provisioners_gate"
+}
+}
+
+resource "aws_route_table" Proute_table"{
+vpc_id=aws.vpc.Test.id
+route{
+cidr_block="0.0.0.0/0"
+gateway_id = aws_internet_gateway.Pgateway.id
+}
+}
+
+resource "aws_route_table_association" "Passociation"{
+subnet_id=aws_subnet.Test1.id
+route_table_id =aws_route_table.Proutetabel.id
+}
+
+
+resource "aws_security_group" "Psecurity"{
+
+vpc_id=aws.vpc.Test.id
+
+ingress{
+from_port =80
+to_port= 80
+protocol= "tcp"
+cidr_blocks =["0.0.0.0/0"]
+}
+
+ingress{
+description ="ssh"
+from_port=22
+tp_port=80
+protcol="tcp"
+cidr_block=["0.0.0.0/0"]
+}
+
+egress{
+from_port=0
+t_port=0
+protocol ="-1"
+cdir_blocks=["0.0.0.0/0"]
+}
+
+tags{
+Name=  "Provisionerroue_association"
+}
+}
+
+
+resource "aws_instance" "server" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.Psecurity.id]
+  subnet_id              = aws_subnet.Test1.id
+}
