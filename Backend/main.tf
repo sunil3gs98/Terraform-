22360f52b14874842d98cdf4580d114e19b54812
Sunil Gowda S <64064177+sunil3gs98@users.noreply.github.com>
2026-01-23 23:40:46 +0530

    Update main.tf

--- Backend/main.tf
+++ Backend/main.tf
@@ -9,3 +9,14 @@ resource "aws_instance" "BackedTest" {
     instance_type = "t2.micro"
   
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
